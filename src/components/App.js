7bc473fa1f6356e29f23a41d14f79d0a8edd5f4d
allanstar-byte <allanmutisya263@gmail.com>
2022-07-14 04:58:43 +0300

    Import / Export

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import About from "../components/About";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return ( <
+        div >
+        <
+        NavBar / >
+        <
+        Home / >
+        <
+        About / >
+        <
+        /div>
+    );
 }
 
 export default App;
