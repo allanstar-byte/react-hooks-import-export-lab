f4907c64b7b47dfdcf275e44a3d6fa78d0d14300
allanstar-byte <allanmutisya263@gmail.com>
2022-10-18 12:09:24 +0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import {image} from '../data/user'
+
 function Footer(){
     return(
         <div>
             <p>I'm a footer buddy</p>
+            <img src={image}/>
         </div>
     )
 }
